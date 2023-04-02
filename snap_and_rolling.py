d93a226604223376f572bb0a52edda41f40cc57f
massnomis <massnomis@gmail.com>
2023-04-02 15:48:22 -0400

    Update snap_and_rolling.py

--- snap_and_rolling.py
+++ snap_and_rolling.py
@@ -25,7 +25,107 @@ import math
 st.set_page_config(layout="wide")
 
 
-st.write("""
+st.title("""
+Compound LTV Changes and Analysis
+""")
+
+st.markdown("""
+There has been frequent discussion recently on analyzing risk parameters for digital assets in lending protocols. More an art than a science, these risk parameters are often calculated based on a variety of internal and external datapoints and occurrences. However, there is no reason why risk models, and LTV ratios in particular, should not be scientifically and mathematically monitored. As an initial try to find a best fit LTV ratio for a variety of crypto assets, three unique formulas were attempted to model the crypto-assets LTV changes. 
+
+
+Prior to showing the results, it is imperative to introduce the context, definitions, and formulas used in the analysis. 
+""")
+st.title("""
+Tokens
+""")
+st.markdown("""
+
+All analysis in this report was done on Compound V2. Eight specific tokens (notably, “long tail assets”) were researched and analyzed in this model: ZRX, UNI, YFI, MKR, AAVE, LINK, BAT, SUSHI. MKR, YFI, AAVE, LINK, SUSHI tokens specifically were part of a push to add some longer tail assets to Compound in 2021. BAT, COMP, ZRX are from the earlier days of Compound and UNI is from the middle, right as the UNI token launched. The reasoning as to why these tokens were chosen is due to their small marginal differences (across liquidity, volume, market cap) can show a lot on the underlying metrics of LTV compared to ETH or USDC which are volume and liquidity intensive assets. However, it is notable to share several bad data errors which came up throughout the analysis. Keep these in mind as the results are discussed. Regarding SUSHI, its liquidity will be weaker on Uniswap V2 and Uniswap V3, as Sushiswap is the main venue for this token and thus most of its trading and volume occurs there. On AAVE, its liquidity drops significantly as the AAVE Safety module moved a significant amount of liquidity towards Balancer, while BAT and ZRX lacks liquidity data altogether from its earlier blocks.
+""")
+st.title("""
+Formulas and Definitions
+""")
+st.markdown("""
+
+Three formulas were used in this analysis. Below are their definitions, the logic behind them, and how they were used throughout the report. 
+""")
+st.title("""
+
+Definitions used throughout the analysis:
+""")
+st.code("""
+'30_day_volatility' = close.rolling(30).std/close (the standard deviation of the rolling close)
+""")
+st.code("""
+'USDT_slippage_combined' = USDT equivalent quote volume for Uniswap V2 and Uniswap V3 (added)
+""")
+st.code("""
+
+'v2_v3_ratio_USD' = (v2 liquidity (USDT) at 5%) / (v3 Liquidity (USDT) at 5%)
+    """)
+st.code("""
+
+'USDT_slippage_combined_volatility' = ['USDT_slippage_combined'].rolling(30).std() / ['USDT_slippage_combined'].rolling(30).mean() 
+
+""")
+st.markdown("""
+
+
+
+
+The formulas are the following: 
+
+
+
+Formula A (“standard view”):
+""")
+
+st.code("""
+ ((['30_day_volatility'] ** 2) / ['USDT_slippage_combined']) """)
+st.markdown("""
+The logic behind this formula is that volatility and slippage are the main drivers for LTV changes, with volatility being weighted higher. Meanwhile, slippage is able to “control” and “buffer” liquidity.
+
+
+
+
+Formula C (“Stickiness By Volatility”)
+""")
+st.code("""
+
+
+(((['30_day_volatility'] ** 0.5) / (['USDT_slippage_combined_volatility']) ) / ['USDT_slippage_combined']) 
+""")
+st.markdown("""
+
+This formula is very similar to A and B, but includes a more quantitative measure on the reaction of liquidity depth towards volatility. 
+
+
+As you can see, all formulas are relatively similar, yet with acute differences that create practical reasoning for which to use to estimate the LTV based on its formulas. The subsequent report showcases which formula has a best fit correlation (to the naked eye) with the formula and LTV ratio. 
+""")
+st.title("""
+The Results
+""")
+st.markdown("""
+
+As discussed, the aim of this analysis is to estimate (or even predict) LTV changes based on the varied formulas. It is logical each asset has a different preferred formula than its peers, however these three formulas best exemplify that volatility and slippage are the main drivers of LTV changes, yet some assets need a preference for their stickiness of their liquidity or their liquidity depth. These results are quite informative to understand the dynamics of these assets. 
+
+""")
+st.title("""
+
+
+
+
+
+
+
+
+
+Takeaways
+
+""")
+st.markdown("""
+
+Overall, Formula C has the best fit correlation to the LTV ratio. This is because it is able to capture the stickiness of liquidity depth towards volatility. This is especially true for the longer tail assets, as they are more volatile and have less liquidity depth.
 
 """)
 
